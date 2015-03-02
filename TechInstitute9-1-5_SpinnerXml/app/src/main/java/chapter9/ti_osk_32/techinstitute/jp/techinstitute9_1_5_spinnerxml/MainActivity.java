281f611b2900cda42801f05ee8d96911bb531ec3
Yasutaka Kawamoto <kwmt27@gmail.com>
2015-03-02 12:07:06 +0900

    スピナーの項目が選択された場合、トーストを表示する

--- TechInstitute9-1-5_SpinnerXml/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/techinstitute9_1_5_spinnerxml/MainActivity.java
+++ TechInstitute9-1-5_SpinnerXml/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/techinstitute9_1_5_spinnerxml/MainActivity.java
@@ -1,9 +1,13 @@
 package chapter9.ti_osk_32.techinstitute.jp.techinstitute9_1_5_spinnerxml;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,30 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // activity_mainからidがspinnerのインスタンスを取得してspinner変数に格納する。
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+
+        // インスタンスspinnerに項目が選択されたときの動作を設定する。
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 項目が選択されたときの実際の動作を記述する。
+
+                // 選択された項目から文字を取り出す
+                String str = ((Spinner)parent).getSelectedItem().toString();
+
+                // 取り出された文字をトーストで表示する。
+                Toast.makeText(getApplicationContext(), str + "が選択されました", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
     }
 
 
