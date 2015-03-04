d10ee098a43f30131e395455d9f744474f51d2cc
Yasutaka Kawamoto <kwmt27@gmail.com>
2015-03-04 18:35:24 +0900

    メインスレッドからネットワークアクセスしようとすると、エラーまで
    ・URLConnection
    ・Internetパーミッション
    ・メインスレッドからのネットワークアクセスエラー

--- JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/WeatherForecastDetailActivity.java
+++ JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/WeatherForecastDetailActivity.java
@@ -1,18 +1,59 @@
 package chapter9.ti_osk_32.techinstitute.jp.jsonweatherforecast;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class WeatherForecastDetailActivity extends ActionBarActivity {
+
+    // 天気予報API URL
+    String weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Osaka&mode=json&units=metric&cnt=2";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast_detail);
+
+        StringBuilder content = new StringBuilder();
+
+        try {
+            // urlオブジェクトを作成
+            URL url = new URL(weatherUrl);
+
+            // urlConnectionを作成
+            URLConnection urlConnection = url.openConnection();
+
+            // データを取得
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+
+            // データを一行ずつ読み込んで、contentに追記する。
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line);
+            }
+
+            bufferedReader.close();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        // 取得結果を確認
+        Log.d("TAG", content.toString());
+
     }
+
+
 
 
     @Override
