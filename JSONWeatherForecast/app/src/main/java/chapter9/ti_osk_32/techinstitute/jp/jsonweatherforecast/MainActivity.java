706875d439ef332db4ff44c06404d4b2fbd9facd
Yasutaka Kawamoto <kwmt27@gmail.com>
2015-03-04 16:32:31 +0900

    ボタンを押したら天気予報詳細画面を表示するように変更

--- JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/MainActivity.java
+++ JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/MainActivity.java
@@ -1,9 +1,12 @@
 package chapter9.ti_osk_32.techinstitute.jp.jsonweatherforecast;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WeatherForecastDetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
