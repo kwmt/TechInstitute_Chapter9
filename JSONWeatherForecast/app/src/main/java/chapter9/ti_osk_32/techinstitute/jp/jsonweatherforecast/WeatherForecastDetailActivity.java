6cdac19429b3a3951778760682d23a26d6399f83
Yasutaka Kawamoto <kwmt27@gmail.com>
2015-03-04 19:53:34 +0900

    画像ダウンロード

--- JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/WeatherForecastDetailActivity.java
+++ JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/WeatherForecastDetailActivity.java
@@ -1,11 +1,14 @@
 package chapter9.ti_osk_32.techinstitute.jp.jsonweatherforecast;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -13,6 +16,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -22,6 +27,9 @@ public class WeatherForecastDetailActivity extends ActionBarActivity {
 
     // 天気予報API URL
     String weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Osaka&mode=json&units=metric&cnt=2";
+
+    // 天気予報API 画像ダウンロード用 URL
+    String imageUrl = "http://openweathermap.org/img/w/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,21 +83,28 @@ public class WeatherForecastDetailActivity extends ActionBarActivity {
         }
 
 
-        // このメソッドは非同期処理が終わった後に呼び出さる。
+        // このメソッドは非同期処理が終わった後に呼び出される。
         @Override
         protected void onPostExecute(String result) {
             try {
+
                 JSONObject jsonObject = new JSONObject(result);
                 JSONArray list = jsonObject.getJSONArray("list");
                 JSONObject tomorrow = list.getJSONObject(0);
                 int tempMax = tomorrow.getJSONObject("temp").getInt("max");
                 int tempMin = tomorrow.getJSONObject("temp").getInt("min");
+                String weatherIcon = tomorrow.getJSONArray("weather").getJSONObject(0).getString("icon");
 
                 TextView tempMaxTextView = (TextView)findViewById(R.id.tempMax);
                 TextView tempMinTextView = (TextView)findViewById(R.id.tempMin);
 
                 tempMaxTextView.setText(String.valueOf(tempMax));
                 tempMinTextView.setText(String.valueOf(tempMin));
+
+                // 画像をダウンロードする。
+                DownloadImageTask task = new DownloadImageTask();
+                task.execute(imageUrl + weatherIcon + ".png");
+
 
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -99,7 +114,58 @@ public class WeatherForecastDetailActivity extends ActionBarActivity {
 
     }
 
+    /**
+     * 画像ダウンロード用のAsyncTask
+     */
+    public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
 
+
+        public DownloadImageTask() {
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            InputStream inputStream = null;
+            Bitmap bitmap = null;
+            StringBuilder content = new StringBuilder();
+
+            try {
+                // urlオブジェクトを作成
+                URL url = new URL(urls[0]);
+
+                // urlConnectionを作成
+                URLConnection urlConnection = url.openConnection();
+
+                // データを取得
+                inputStream = urlConnection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return bitmap;
+        }
+
+
+        // このメソッドは非同期処理が終わった後に呼び出さる。
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            ImageView imageView = (ImageView)findViewById(R.id.weatherIcon);
+            imageView.setImageBitmap(result);
+
+        }
+
+
+    }
 
 
     @Override
