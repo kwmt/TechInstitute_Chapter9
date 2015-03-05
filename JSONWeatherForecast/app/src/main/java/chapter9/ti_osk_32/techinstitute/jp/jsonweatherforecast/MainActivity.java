b120bbefc761bab324bff5add908c512617baac5
Yasutaka Kawamoto <kwmt27@gmail.com>
2015-03-05 09:35:39 +0900

    最初の画面に都道府県リストを表示させて、それぞれの都道府県をタップするとそれぞれの都道府県の天気予報詳細がでるように変更

--- JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/MainActivity.java
+++ JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/MainActivity.java
@@ -6,15 +6,53 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView listView;
+    private List<String> dataList = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+
+    private Map<String, String> prefectures = new HashMap<String, String>();
+    private ArrayList<String> prefectureKeys;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
+
+        setupPrefectures();
+        setAdapters();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ListView listView = (ListView) parent;
+                // クリックされたアイテムを取得します
+//                String item = (String) listView.getItemAtPosition(position);
+                String key = prefectureKeys.get(position);
+                Toast.makeText(MainActivity.this, key, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this, WeatherForecastDetailActivity.class);
+                intent.putExtra("prefecture", key);
+                startActivity(intent);
+
+
+            }
+        });
 
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +64,26 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
+    private void setupPrefectures() {
+        prefectures.put("tokyo", "東京");
+        prefectures.put("osaka", "大阪");
+
+        prefectureKeys = new ArrayList<String>(prefectures.keySet());
+    }
+
+    protected void setAdapters(){
+        adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                dataList);
+        listView.setAdapter(adapter);
+
+        for(Map.Entry<String, String> e : prefectures.entrySet()) {
+            adapter.add(e.getValue());
+        }
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/WeatherForecastDetailActivity.java
+++ JSONWeatherForecast/app/src/main/java/chapter9/ti_osk_32/techinstitute/jp/jsonweatherforecast/WeatherForecastDetailActivity.java
@@ -26,7 +26,7 @@ import java.net.URLConnection;
 public class WeatherForecastDetailActivity extends ActionBarActivity {
 
     // 天気予報API URL
-    String weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Osaka&mode=json&units=metric&cnt=2";
+    String weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=2&q=";
 
     // 天気予報API 画像ダウンロード用 URL
     String imageUrl = "http://openweathermap.org/img/w/";
@@ -36,9 +36,16 @@ public class WeatherForecastDetailActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast_detail);
 
+
+        String prefecture = getIntent().getStringExtra("prefecture");
+        String prefectureWeatherUrl = weatherUrl + prefecture;
+
+        TextView prefectureTextView = (TextView)findViewById(R.id.prefecture);
+        prefectureTextView.setText(prefecture);
+
         // 非同期でネットワークにアクセスする。
         AsyncHttpRequest task = new AsyncHttpRequest();
-        task.execute(weatherUrl);
+        task.execute(prefectureWeatherUrl);
     }
 
 
